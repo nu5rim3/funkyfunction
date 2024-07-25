3b6273250538969b4eb99733187d17c66aa9663c
nu5rim3 <nusrisamath@gmail.com>
2024-07-25 20:26:20 +0530

    updated: change company name

--- src/sections/header.tsx
+++ src/sections/header.tsx
@@ -12,10 +12,10 @@ export default function Header() {
     <header className="dark:bg-darkBg inset-0 flex min-h-[80dvh] w-full flex-col items-center justify-center bg-white bg-[linear-gradient(to_right,#80808033_1px,transparent_1px),linear-gradient(to_bottom,#80808033_1px,transparent_1px)] bg-[size:70px_70px]">
       <div className="mx-auto w-container max-w-full px-5 py-[110px] text-center lg:py-[150px]">
         <h1 className={`text-5xl md:text-7xl lg:text-9xl ${bungee.className}`}>
-          funky()
+          funky();
         </h1>
         <p className="my-12 mt-8 text-lg font-normal leading-relaxed md:text-xl lg:text-2xl lg:leading-relaxed">
-        Unleash the Funk in Your Code!
+          Unleash the Funk in Your Code!
           <br /> Transforming Ideas into Dynamic Software Solutions.
         </p>
         <Button
